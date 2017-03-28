86a0318f0b6ebf8b779a39502298710dd2ef63cc
bhavya95 <bhavyasaini95@gmail.com>
2017-03-28 19:10:12 +0530

    companyView change

--- application/views/companyView.php
+++ application/views/companyView.php
@@ -1,14 +1,42 @@
 
                 <h1> Add Company </h1>
-                <form>
-                    
-
-                    <input type="text" name="Company">
-
-                
-                    <input type="text" name="Company">
-                    <input type="text" name="Company">
-                    <input type="text" name="Company">
-                    <input type="text" name="Company">
-                </form>
-           
+                <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <strong>Company</strong>
+                                    <small>Form</small>
+                                </div>
+                                <div class="card-block">
+                                    <div class="form-group">
+                                        <label for="company">Company's Name</label>
+                                        <input type="text" class="form-control" id="company" placeholder="Enter company name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">About</label>
+                                        <textarea placeholder="Enter text here" rows = "4" class="form-control"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="website">Website</label>
+                                        <input type="website" class="form-control" id="website" placeholder="Enter Company's Website ">
+                                    </div>
+                                    <div class="row">
+                                        <div class="form-group col-sm-8">
+                                            <label for="city">City</label>
+                                            <input type="text" class="form-control" id="city" placeholder="Enter your city">
+                                        </div>
+                                        <div class="form-group col-sm-4">
+                                            <label for="postal-code">Postal Code</label>
+                                            <input type="text" class="form-control" id="postal-code" placeholder="Postal Code">
+                                        </div>
+                                    </div>
+                                    <!--/row-->
+                                    <div class="form-group">
+                                        <label for="country">Country</label>
+                                        <input type="text" class="form-control" id="country" placeholder="Country name">
+                                    </div>
+                                    <div>
+                                        <button type="button" class="btn btn-primary">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
